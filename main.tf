dc6125e5c62d546276d810b821657a604d7ab3d2
Paul McIntire <pmcintire@pauls-mbp.localdomain>
2023-11-06 13:04:07 -0800

    add mig

--- main.tf
+++ main.tf
@@ -30,34 +30,34 @@ resource "google_compute_instance" "nginx_instance" {
   }
 }
 
-module "webservers" {
-  source = "./modules/webservers"
-  project_id = var.project-id
-  server_settings = var.environment_instance_settings
-  region = var.region
-  zone = var.zone
-  network_interface = {
-      network = data.google_compute_network.default.self_link,
-      subnetwork = google_compute_subnetwork.subnet-1.self_link
-      }
-}
+# module "webservers" {
+#  source = "./modules/webservers"
+#  project_id = var.project-id
+#  server_settings = var.environment_instance_settings
+#  region = var.region
+#  zone = var.zone
+#  network_interface = {
+#      network = data.google_compute_network.default.self_link,
+#      subnetwork = google_compute_subnetwork.subnet-1.self_link
+#      }
+#}
 
 ## DB
-resource "google_compute_instance" "mysqldb" {
-  name         = "mysqldb"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
-}
+#resource "google_compute_instance" "mysqldb" {
+#  name         = "mysqldb"
+#  machine_type = var.environment_machine_type[var.target_environment]
+#  labels = {
+#    environment = var.environment_map[var.target_environment]
+#  }
+#  
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#    }
+#  }
+#
+#  network_interface {
+#    network = data.google_compute_network.default.self_link
+#    subnetwork = google_compute_subnetwork.subnet-1.self_link
+#  }  
+#}
---
+++ mig.tf
@@ -0,0 +1,20 @@
+module "vm_instance_template" {
+  source  = "terraform-google-modules/vm/google//modules/instance_template"
+  version = "7.9.0"
+  project_id = var.project_id
+  machine_type = "e2-micro" 
+  subnetwork = google_compute_subnetwork.subnet-1.name
+  service_account { 
+    email = var.instance_service_account
+    scopes = []
+  }
+}
+
+module "vm_mig" {
+  source  = "terraform-google-modules/vm/google//modules/mig"
+  version = "7.9.0"
+  project_id = var.project-id
+  region = var.region
+  hostname = "web-server" 
+  instance_template = module.vm_instance_template.self_link
+}
